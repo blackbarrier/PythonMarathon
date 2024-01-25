078d6a63ac6161a3c22b41a9f6e81ee90306fa29
LeandroBarrera <leann.barrera@gmail.com>
2024-01-25 12:16:38 -0300

    proyecto1

---
+++ 8JeringozaFinal.py
@@ -0,0 +1,33 @@
+#Traductor de jeringoza
+#Agregar runa p despues de cada vocal
+#Repetir la vocal
+
+L=["perro","gato","canario","elefante","lobo","delfin"]
+
+def traducir(L):
+    string=""    
+
+    for palabra in L:
+
+        palabraTraducida=""
+
+        for letra in palabra:
+            if letra in "aeiouAEIOU":
+                letra=letra+"p"+letra
+            palabraTraducida+=letra
+
+        if palabra==L[0]:
+            palabraTraducida=palabraTraducida.capitalize()
+            palabraTraducida+=", "
+        elif palabra==L[-1]:
+            palabraTraducida+="."
+        else:
+            palabraTraducida+=", "
+
+        string+=palabraTraducida        
+
+    print(string)
+
+
+
+traducir(L)
