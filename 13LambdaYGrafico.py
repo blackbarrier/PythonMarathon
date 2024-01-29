c60b8ef564552296101a3899a474fb28a678530e
blackbarrier <leann.barrerag@gmail.com>
2024-01-29 09:25:42 -0300

    29/1

--- 13LambdaYGrafico.py
+++ 13LambdaYGrafico.py
@@ -1,1 +1,18 @@
-2
+#Expliquemos funciones lambda
+#Son como funciones matematicas
+
+#Basicamente esta funcion en particular
+#hace el cuadrado de x.
+f = lambda x: x**2
+
+#Cuando la llamo reemplazo f(x)
+res=f(3)
+print(res)
+
+
+
+#Otro ejemplo
+
+#En este caso devuelve 1 si x es >=10 sino -1
+f= lambda x: 1 if x >=10 else -1
+print(f(11))
